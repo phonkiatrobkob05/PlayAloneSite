e5e84b38a9a97aad98366ed09d7e72607b5e9cf1
phonkiatrobkob05 <phonkiatrobkob05@gmail.com>
2025-08-15 02:48:23 +0700

    hamburger menu

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,25 +1,56 @@
-import { Link, Element } from 'react-scroll';
+import { useState } from 'react';
+import { Link } from 'react-scroll';
+import { Squash as Hamburger } from 'hamburger-react';
 
-function Navbar () {
+function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const closeMenu = () => setIsOpen(false);
+
   return (
     <nav className="bg-black-figma p-4">
       <div className="container flex justify-between items-center font-bold text-2xl">
-        <div className="text-white text-lg mr-0">
-          <img src="/playalonelogo.png" alt="" className="w-[100px] md:w-[250px]"/>
+        <div className="text-white text-lg">
+          <img
+            src="/playalonelogo.png"
+            alt="Logo"
+            className="w-[100px] md:w-[250px]"
+          />
         </div>
-        <ul className="flex space-x-4 text-white-figma">
-          <li className='text-xl cursor-pointer'>
+        <ul className="hidden md:flex space-x-6 text-white-figma">
+          <li className="text-xl cursor-pointer">
             <Link to="Home" smooth={true} duration={500}>Home</Link>
           </li>
-          <li className='text-xl cursor-pointer'>
+          <li className="text-xl cursor-pointer">
             <Link to="TaKhon" smooth={true} duration={500}>TaKhon</Link>
           </li>
-          <li className='text-xl cursor-pointer'>
+          <li className="text-xl cursor-pointer">
             <Link to="Contact" smooth={true} duration={500}>itch.io</Link>
+          </li>
+        </ul>
+        <div className="md:hidden text-white-figma">
+          <Hamburger toggled={isOpen} toggle={setIsOpen} />
+        </div>
+      </div>
+      <div
+        className={`md:hidden overflow-hidden transition-all duration-500 ease-in-out ${
+          isOpen ? 'max-h-60 opacity-100 translate-y-0' : 'max-h-0 opacity-0 -translate-y-4'
+        }`}
+      >
+        <ul className="flex flex-col items-center space-y-4 mt-4 text-white-figma">
+          <li className="text-xl cursor-pointer">
+            <Link to="Home" smooth={true} duration={500} onClick={closeMenu}>Home</Link>
+          </li>
+          <li className="text-xl cursor-pointer">
+            <Link to="TaKhon" smooth={true} duration={500} onClick={closeMenu}>TaKhon</Link>
+          </li>
+          <li className="text-xl cursor-pointer">
+            <Link to="Contact" smooth={true} duration={500} onClick={closeMenu}>itch.io</Link>
           </li>
         </ul>
       </div>
     </nav>
   );
 }
+
 export default Navbar;
