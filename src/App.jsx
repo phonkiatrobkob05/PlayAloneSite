d420f9004b21a456623407abee0783c5841b0547
phonkiatrobkob05 <phonkiatrobkob05@gmail.com>
2025-08-15 22:41:02 +0700

    middle the left  box

--- src/App.jsx
+++ src/App.jsx
@@ -8,15 +8,20 @@ function App() {
     <div className='bg-[url(/bgplayalone.png)] h-screen w-screen bg-cover bg-center text-white'>
       <Navbar />
       <Element name="Home" className='flex h-full items-center p-8 w-full'>
-            <div className='flex flex-col w-full justify-center p-8'><h1 className='text-3xl font-bold'>PlayALONE</h1>
-              <p className='text-2xl'>is a passionate indie game</p>
-                <p>development team dedicated to</p>
-                <p>crafting unique, story-driven</p>
-                <p>experiences</p>
-              <button className='text-3xl p-2 bg-highlight rounded-2xl mt-10 w-50'>Follow us</button>
-                
-                </div>
-            <div className='flex flex-col w-full'></div>
+        <div id='left' className='w-full h-full flex justify-center items-center'>
+          <div id='Text' className=''>
+            <div id='Header' className=''>
+              <h1 id='PlayALONE' className='text-3xl font-bold'>PlayALONE</h1>
+              <p id='isapassionate'> is a passionate indie game <br />
+              development team dedicated to <br />
+              crafting unique, story-driven <br />
+              experiences</p>
+            </div>
+            <button className='text-3xl font-bold p-2 bg-highlight rounded-xl mt-10 w-50 cursor-pointer transition duration-400 ease-in-out hover:bg-highlight-hover '>
+              <a href="https://www.facebook.com/profile.php?id=61579360801974">Follow us</a></button>
+          </div>
+        </div>
+        <div className='flex flex-col w-full'></div>
       </Element>
       <Footer />
     </div>
